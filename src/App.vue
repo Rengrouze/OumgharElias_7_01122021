91d2849407e8f943e9626fc108d9f9b9ec37cc9f
Elias Oumghar <eoumghar@gmail.com>
2022-01-17 13:06:28 +0100

    managed to install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
-import Header from "./components/Header.vue";
-import Feed from "./components/Feed.vue";
-import Footer from "./components/Footer.vue";
-</script>
-
 <template>
-   <router-view />
-   <Header></Header>
-   <Feed></Feed>
-   <Footer></Footer>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <style>
 #app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-align: center;
-   color: #2c3e50;
-   position: static;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
